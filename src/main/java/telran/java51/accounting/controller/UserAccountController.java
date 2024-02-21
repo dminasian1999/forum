8e2e921fa23adede6b2b22baf07250bfbb94753c
LENOVO <LENOVO@192.168.1.101>
2024-02-21 04:37:59 +0200

    get header authorization

--- src/main/java/telran/java51/accounting/controller/UserAccountController.java
+++ src/main/java/telran/java51/accounting/controller/UserAccountController.java
@@ -1,11 +1,14 @@
 package telran.java51.accounting.controller;
 
+import java.util.Base64;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +31,13 @@ public class UserAccountController {
 		return userAccountService.register(userRegisterDto);
 	}
 	
-
+	@PostMapping("/login")
+	public UserDto login(@RequestHeader("Authorization") String token) {
+		token = token.split(" ")[1];
+		String credentials = new String(Base64.getDecoder().decode(token));
+		return userAccountService.getUser(credentials.split(":")[0]);
+	}
+	
 	@GetMapping("/user/{login}")
 	public UserDto getUser(@PathVariable String login) {
 		return userAccountService.getUser(login);
